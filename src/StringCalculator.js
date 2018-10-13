a03f537044001e6d719983b22494a533076843de
Thomas Ari Bech <thomasb17@ru.is>
2018-10-13 17:29:51 +0000

    Implemnted add to support a string with a number

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,7 +1,11 @@
 // src/StringCalculator.js
 
 function add(numbers) {
-	return 0;
+	if (numbers === "") {
+		return 0;
+	}
+	
+	return parseInt(numbers);
 }
 
 module.exports = add;
--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -5,3 +5,7 @@ const add = require("./StringCalculator");
 it("should return 0 on a empty string", () => {
 	expect(add("")).toBe(0);
 });
+
+it("should return the number on a string with a single number", () => {
+	expect(add("2")).toBe(2);
+});
