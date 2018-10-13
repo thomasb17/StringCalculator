23f8fc6d80bb3946364f117257f5924755f58629
Thomas Ari Bech <thomasb17@ru.is>
2018-10-13 17:35:18 +0000

    Implemented add to support up to two numbers

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -4,7 +4,12 @@ function add(numbers) {
 	if (numbers === "") {
 		return 0;
 	}
-	
+
+	const n = numbers.split(",");
+	if (n.length > 1) {
+		return parseInt(n[0]) + parseInt(n[1]);
+	}
+
 	return parseInt(numbers);
 }
 
--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -9,3 +9,7 @@ it("should return 0 on a empty string", () => {
 it("should return the number on a string with a single number", () => {
 	expect(add("2")).toBe(2);
 });
+
+it("should return the sum of two numbers in a string", () => {
+	expect(add("1,2")).toBe(3);
+});
